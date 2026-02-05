91b6987f1d07b14c583654380e4671e6861cb8e7
Mr. Ohms <36096262+BAmato@users.noreply.github.com>
2026-02-04 19:41:27 -0700

    Add autonomous FSM with test mode chooser

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -5,10 +5,42 @@
 #include "Robot.h"
 
 Robot::Robot() {}
+void Robot::RobotInit() {
+  m_enableTestModeChooser.SetDefaultOption("Disabled", false);
+  m_enableTestModeChooser.AddOption("Enabled", true);
+  frc::SmartDashboard::PutData("Enable Test Mode", &m_enableTestModeChooser);
+}
 void Robot::RobotPeriodic() {}
 
 void Robot::AutonomousInit() {}
-void Robot::AutonomousPeriodic() {}
+void Robot::AutonomousPeriodic() {
+  if (m_enableTestModeChooser.GetSelected()) {
+    m_autoState = AutoState::TEST_MODE;
+  } else if (m_autoState == AutoState::TEST_MODE) {
+    m_autoState = AutoState::START;
+  }
+
+  switch (m_autoState) {
+    case AutoState::START:
+      // Initialize sensors and read NetworkTables for field setup.
+      m_autoState = AutoState::SWEEP;
+      break;
+    case AutoState::SWEEP:
+      // Default behavior: follow a hardcoded snake path to collect items.
+      break;
+    case AutoState::RETRIEVE_BOX:
+      // Interrupt: approach and latch onto a box detected by vision.
+      break;
+    case AutoState::DUMP_MATERIALS:
+      // Interrupt: return to start and empty the bins.
+      break;
+    case AutoState::TEST_MODE:
+      // Debug-only calls: uncomment a single action to test hardware.
+      // testMotor();
+      // driveOneMeter();
+      break;
+  }
+}
 
 void Robot::TeleopInit() {}
 void Robot::TeleopPeriodic() {}
